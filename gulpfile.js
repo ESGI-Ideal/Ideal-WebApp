ce9c6a25e624e6396e33cd16b4042636c5c48f46
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2018-04-28 12:01:23 +0200

    little enhancement

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@ const mainBowerFiles = require('main-bower-files');
 const noop = require("gulp-noop");
 const plumber = require("gulp-plumber");
 const debug = require('gulp-debug');
+const preservetime = require('gulp-preservetime');
+const hashsum = require("gulp-hashsum");
 const browserSync = require('browser-sync').create();
 const options = require("minimist")(process.argv.slice(2));
 
+// Copy third party libraries from /node_modules into /vendor
 gulp.task('deps', function() {
     return gulp.src(mainBowerFiles({checkExistence: true}))
         .pipe(!options.production ? plumber() : noop())
@@ -17,18 +20,26 @@ gulp.task('deps', function() {
         .pipe(gulp.dest('./vendor/libs'));
 });
 
-// Copy third party libraries from /node_modules into /vendor
+// Copy source files from /src into /vendor
 gulp.task('vendor', function() {
     gulp.src('./src/**/*')
         .pipe(!options.production ? plumber() : noop())
         .pipe(debug({title: 'src-debug'}))
         //.pipe(3rd)
         //.pipe(!options.production ? plumber.stop() : noop())
-        .pipe(gulp.dest('./vendor'));
+        .pipe(gulp.dest('./vendor'))
+        .pipe(preservetime());
 });
 
 // Default task
 gulp.task('default', ['vendor', 'deps']);
+
+//for
+gulp.task('dist', ['deps', 'vendor'], function() {
+    return gulp.src('./vendor/**/*')
+        .pipe(hashsum({dest: "vendor", force: true, hash: "md5"}))
+        .pipe(hashsum({dest: "vendor", force: true, hash: "sha1"}));
+});
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
