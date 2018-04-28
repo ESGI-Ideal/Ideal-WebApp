ac23af7dc72ae76f47f80c9d340eb4b23fdbd745
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2018-04-28 14:51:13 +0200

    Correct include with "min" version of dependencies Add clean tasks Add support of "map" for devel Correct links in html (with defer)

--- gulpfile.js
+++ gulpfile.js
@@ -7,39 +7,66 @@ const plumber = require("gulp-plumber");
 const debug = require('gulp-debug');
 const preservetime = require('gulp-preservetime');
 const hashsum = require("gulp-hashsum");
+const sourcemaps = require('gulp-sourcemaps');
+//const filter = require('gulp-filter');
+const ignore = require('gulp-ignore');
+const del = require('del');
 const browserSync = require('browser-sync').create();
 const options = require("minimist")(process.argv.slice(2));
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('deps', function() {
-    return gulp.src(mainBowerFiles({checkExistence: true}))
+    return gulp.src(mainBowerFiles({
+        checkExistence: true,
+        env: (options.production && !options.development) ? "production" : "development"
+    }))
         .pipe(!options.production ? plumber() : noop())
+        //.pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(options.production ? ignore("*.map") : noop())
         .pipe(debug({title: 'deps-debug'/*, showFiles: false*/}))
         //.pipe(3rd)
-        //.pipe(!options.production ? plumber.stop() : noop())
+        //.pipe(sourcemaps.write())
+        .pipe(!options.production ? plumber.stop() : noop())
         .pipe(gulp.dest('./vendor/libs'));
 });
 
 // Copy source files from /src into /vendor
-gulp.task('vendor', function() {
+gulp.task('build', function() {
     gulp.src('./src/**/*')
         .pipe(!options.production ? plumber() : noop())
         .pipe(debug({title: 'src-debug'}))
+        .pipe(sourcemaps.init())
         //.pipe(3rd)
-        //.pipe(!options.production ? plumber.stop() : noop())
+        .pipe(sourcemaps.write())
+        .pipe(!options.production ? plumber.stop() : noop())
         .pipe(gulp.dest('./vendor'))
         .pipe(preservetime());
 });
+//alias for legacy
+gulp.task('vendor', ['build']);
 
 // Default task
 gulp.task('default', ['vendor', 'deps']);
 
-//for
-gulp.task('dist', ['deps', 'vendor'], function() {
+//for release
+gulp.task('dist', ['deps', 'build'], function() {
     return gulp.src('./vendor/**/*')
         .pipe(hashsum({dest: "vendor", force: true, hash: "md5"}))
         .pipe(hashsum({dest: "vendor", force: true, hash: "sha1"}));
 });
+
+//cleaning project dir
+gulp.task('clean:build', function () {
+    return del(['./vendor']);
+});
+
+//cleaning project dir
+gulp.task('clean:dist', function () {
+    return del(['./dist']);
+});
+
+//full clean
+gulp.task('clean', ['clean:build', 'clean:dist']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
